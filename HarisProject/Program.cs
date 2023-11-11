df23acdbbaade19a7161ce4da48e323dee96f16f
ArmanKhalili82 <hykhalili@yahoo.com>
2023-11-11 22:01:05 +0100

    db

--- HarisProject/Program.cs
+++ HarisProject/Program.cs
@@ -2,12 +2,17 @@
 
 while (true)
 {
+    var db = new ApplicationDbContext();
+
     var p = new HarisNumber();
 
     Console.WriteLine("Enter Your Number:");
 
     var n = int.Parse(Console.ReadLine()!);
     var str = p.Convert(n);
+
+    db.stores.Add(new Store() { Number = n, Word = str });
+    await db.SaveChangesAsync();
 
     Console.WriteLine(str);
 }
