60d4a6c2aa73a2697b4859194483d27e7a690935
ArmanKhalili82 <hykhalili@yahoo.com>
2023-11-11 22:23:35 +0100

    savechange

--- HarisProject/Program.cs
+++ HarisProject/Program.cs
@@ -12,14 +12,15 @@ while (true)
     var n = int.Parse(Console.ReadLine()!);
     var str = p.Convert(n);
 
-    db.stores.Add(new Store() { Number = n, Word = str });
-    await db.SaveChangesAsync();
-
     var store = await db.stores.Where(s => s.Number == n).FirstOrDefaultAsync();
     if (store == null)
     {
         db.stores.Add(new Store() { Number = n, Word = str });
+        await db.SaveChangesAsync();
     }
+
+    //db.stores.Add(new Store() { Number = n, Word = str });
+    
 
     Console.WriteLine(str);
 }
